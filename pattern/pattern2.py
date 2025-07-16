f20982859e44bfb6ae807ee358d692807590ab7a
Dinesh kumar <dineshkumartlbupi@gmail.com>
2025-07-16 13:49:49 +0000

    save

--- pattern/pattern2.py
+++ pattern/pattern2.py
@@ -1,3 +1,19 @@
 
+# num=0
+# while num<=5:
+#     print("*")
+#     num +=1 
 
-print("hello this is pattern")s
+# ans 
+# *
+# *
+# *
+# *
+# *
+
+
+# question number two 
+
+# for i in range(1,6):
+# print(i)
+print(1,2,3,4,5)
